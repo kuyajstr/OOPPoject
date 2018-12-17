31677eeddbeda486ceaab886bfbdff1a3e1925b9
kuyajstr <45276232+kuyajstr@users.noreply.github.com>
2018-12-17 09:32:15 +0800

    Added Class

---
+++ wpf/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    public class Person
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public string BirthDate;
+        public string Address;
+
+        public Person(string firstName, string lastName, string middleName = "")
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            MiddleName = MiddleName.First().ToString().ToUpper();
+
+            string[] separatedFirstName = FirstName.Split(' ');
+            string capitalizedFirstName = "";
+
+            if (FirstName.Contains(' '))
+            {
+                for (int i = 0; i < separatedFirstName.Length; i++)
+                {
+                    separatedFirstName[i] = (char.ToUpper((separatedFirstName[i][0]))) +
+                        separatedFirstName[i].ToLower().Substring(1) + " ";
+                    capitalizedFirstName += separatedFirstName[i];
+                }
+            }
+            else capitalizedFirstName = FirstName.First().ToString().ToUpper() + FirstName.Substring(1).ToLower() + " ";
+
+            string [] separatedLastName = LastName.Split(' ');
+            string capitalizedLastName = "";
+
+            if (LastName.Contains(' '))
+            {
+                for (int i = 0; i < separatedLastName.Length; i++)
+                {
+                    separatedLastName[i] = (char.ToUpper((separatedLastName[i][0]))) +
+                        separatedLastName[i].ToLower().Substring(1) + " ";
+                    capitalizedLastName += separatedLastName[i];
+                }
+            }
+            else capitalizedLastName = LastName.First().ToString().ToUpper() + LastName.Substring(1).ToLower();
+
+
+            return capitalizedFirstName + MiddleName + ". " + capitalizedLastName;
+
+        }
+
+        public int GetAge()
+        {
+            return Calculation.CalculateAge(BirthDate);
+        }
+    }
+}
