3a8e968a146b332732c6ef6dbf4c87388db3e1ce
kuyajstr <45276232+kuyajstr@users.noreply.github.com>
2019-01-09 20:33:35 +0800

    Added Windows App

---
+++ OOP_Project/OOP_Project/AddCustomer.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOP_Project
+{
+    /// <summary>
+    /// Interaction logic for AddCustomer.xaml
+    /// </summary>
+    public partial class AddCustomer : Window
+    {
+        public AddCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            string FirstName = this.txtFirstName.Text;
+            string MiddleName = this.txtMiddleName.Text;
+            string LastName = this.txtLastName.Text;
+
+            Person Customer = new Person(FirstName, LastName, MiddleName);
+
+            Customer.BirthDate = this.dpBirthDate.Text;
+            Customer.Address = this.txtAddress.Text;
+
+            AddTransaction AddCustomer = new AddTransaction();
+            AddCustomer.cmbCustomer.Items.Add(Customer.GetFullName());
+
+            AddCustomer.Show();
+            this.Close();
+
+        }   
+
+        private void txtLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
